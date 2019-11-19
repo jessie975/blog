fc5befecac020c733c97eacf3518891822b0d090
jessie <1185922395@qq.com>
2019-11-19 11:15:39 +0800

    11-19

--- coding/test.js
+++ coding/test.js
@@ -1,16 +1,49 @@
-function test(person) {
-  person.age = 26
-  person = {
-    name: 'yyy',
-    age: 30
+const data = [
+  {
+    name: '1',
+    children: [
+      { name: '2',
+        children: [
+          { name: '3' },
+          { name: '4',
+            children: [{ name: '5' }, { name: '6' }]
+          }]
+      },
+      { name: '7',
+        children: [
+          { name: '8',
+            children: [{ name: '9' }]
+          }] }
+    ]
   }
+]
 
-  return person
+// 深度遍历, 使用递归
+// function getName(data) {
+//   const result = []
+//   data.forEach(item => {
+//     const map = data => {
+//       result.push(data.name)
+//       data.children && data.children.forEach(child => map(child))
+//     }
+//     map(item)
+//   })
+//   return result.join(',')
+// }
+
+// 广度遍历, 创建一个执行队列, 当队列为空的时候则结束
+function getName2(data) {
+  const result = []
+  const queue = data
+  while (queue.length > 0) {
+    [...queue].forEach(child => {
+      queue.shift()
+      result.push(child.name)
+      child.children && (queue.push(...child.children))
+    })
+  }
+  return result.join(',')
 }
-const p1 = {
-  name: 'yck',
-  age: 25
-}
-const p2 = test(p1)
-console.log(p1) // -> ?
-console.log(p2) // -> ?
+
+// console.log(getName(data))
+console.log(getName2(data))
