b0876259fabf3b84978da8c843b8baebbf7c73b9
jessie <1185922395@qq.com>
2020-04-22 22:33:31 +0800

    更换名字

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const themeConfig = require('./themeConfig/index.js')
 const head = require('./themeConfig/htmlHeader.js')
 
 module.exports = {
-  title: 'A计划',
+  title: '小木子的博客',
   description: 'jessie打怪升级之路',
   theme: 'reco',
   themeConfig,
