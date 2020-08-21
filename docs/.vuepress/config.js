f114a08edf07cd0111968c2e9b07440ceb4baa87
jessie <1185922395@qq.com>
2020-08-21 20:57:24 +0800

    设计模式读书笔记未完待续

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,4 +7,16 @@ module.exports = {
   theme: 'reco',
   themeConfig,
   head,
+  plugins: {
+    '@vssue/vuepress-plugin-vssue': {
+      platform: 'github-v4', //v3的platform是github，v4的是github-v4
+      locale: 'zh', //语言
+      // 其他的 Vssue 配置
+      owner: 'jessie975', //github账户名
+      repo: 'blog', //github一个项目的名称
+      clientId: '26efc01bddb8550d493f',//注册的Client ID
+      clientSecret: '6d405fe58dd2982e765883a326d66d37514c374c',//注册的Client Secret
+      autoCreateIssue: true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
+    },
+  }
 }
--- docs/.vuepress/themeConfig/nav.js
+++ docs/.vuepress/themeConfig/nav.js
@@ -1,3 +1,5 @@
 module.exports = [
-  { text: '时间轴', link: '/timeLine/', icon: 'reco-date' } 
+  { text: '时间轴', link: '/timeLine/', icon: 'reco-date' }, 
+  { text: 'github', link: 'https://github.com/jessie975', icon: '../public/icon/github.png' }, 
+  { text: '掘金', link: 'https://juejin.im/user/5cfd3e755188254c43467904', icon: '../public/icon/juejin.svg' } 
 ]
